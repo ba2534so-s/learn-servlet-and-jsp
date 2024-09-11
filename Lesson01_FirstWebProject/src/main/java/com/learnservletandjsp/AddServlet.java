b5a82c2f27eb1a1782426e75761660985a71846d
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-11 06:29:01 +0200

    call parseInt for next input

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
@@ -9,6 +9,7 @@ public class AddServlet extends HttpServlet {
 	public void service(HttpServletRequest req, HttpServletResponse res) {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt();
 		
 	}
 
