c15483c2975bea6f93bba8c8004c621765c6eaeb
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-11 06:23:34 +0200

    add service method signature

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
@@ -3,5 +3,9 @@ package com.learnservletandjsp;
 import jakarta.servlet.http.HttpServlet;
 
 public class AddServlet extends HttpServlet {
+	
+	public void service(HttpServerRequest req, HttpServerResponse res) {
+		
+	}
 
 }
