58f71e269beda3b390ba70c3a460539187328f35
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-12 01:38:33 +0200

    add doGet method

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/CubeServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/CubeServlet.java
@@ -1,7 +1,13 @@
 package com.learnservletandjsp;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class CubeServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 
 }
