1c8f9fbb32c7d4f2eb0759b156528bae12b2498b
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-11 22:40:58 +0200

    Comment out service method and create a doPost method for same algorithm

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
@@ -8,18 +8,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		
+
+	/*
+	 * public void service(HttpServletRequest req, HttpServletResponse res) throws
+	 * IOException {
+	 * 
+	 * int i = Integer.parseInt(req.getParameter("num1")); int j =
+	 * Integer.parseInt(req.getParameter("num2"));
+	 * 
+	 * int sum = i + j;
+	 * 
+	 * PrintWriter out = res.getWriter();
+	 * 
+	 * out.println("Result is: " + sum);
+	 * 
+	 * }
+	 */
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
-		
+
 		int sum = i + j;
-		
-		PrintWriter out = res.getWriter(); 
-		
+
+		PrintWriter out = res.getWriter();
+
 		out.println("Result is: " + sum);
-		
+
 	}
 
 }
