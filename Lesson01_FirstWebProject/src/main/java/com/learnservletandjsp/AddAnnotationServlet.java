36e0a47bda1115ac99560de7e1acefd730e3ec57
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-13 23:15:14 +0200

    Get printwriter

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddAnnotationServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddAnnotationServlet.java
@@ -1,5 +1,8 @@
 package com.learnservletandjsp;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,7 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("addAnnotation")
 public class AddAnnotationServlet extends HttpServlet {
 	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		int i = Integer.parseInt(request.getParameter("num1"));
+		int j = Integer.parseInt(request.getParameter("num2"));
+
+		int sum = i + j;
+		
+		PrintWriter out = response.getWriter();
 		
 	}
 
