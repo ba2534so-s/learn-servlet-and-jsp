7295c091c027506a839d4e75cd6444d2f9c5b0c8
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-13 23:12:40 +0200

    add addAnnotation class

---
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddAnnotationServlet.java
@@ -0,0 +1,10 @@
+package com.learnservletandjsp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("addAnnotation")
+public class AddAnnotationServlet extends HttpServlet {
+
+}
