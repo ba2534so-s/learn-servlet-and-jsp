1207372af06d2e8bb0d02c3692126f43aa5d215c
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-13 21:17:22 +0200

    add new sendRedirect without parameter

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddServlet.java
@@ -38,7 +38,7 @@ public class AddServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		session.setAttribute("sum", sum);
 		
-		
+		res.sendRedirect("cube");
 		
 		// res.sendRedirect("cube?sum=" + sum); // URL rewriting
 		
