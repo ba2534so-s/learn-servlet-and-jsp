6d095e0b7756caaf1e9f366a8909a76f03a7ccd6
dev_baran <dev_baran@ubuntu-virtual-machine>
2024-09-13 23:13:53 +0200

    add doGet method signature

--- Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddAnnotationServlet.java
+++ Lesson01_FirstWebProject/src/main/java/com/learnservletandjsp/AddAnnotationServlet.java
@@ -2,9 +2,15 @@ package com.learnservletandjsp;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("addAnnotation")
 public class AddAnnotationServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
 
 }
