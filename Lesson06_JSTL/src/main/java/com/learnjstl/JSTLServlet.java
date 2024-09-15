5d78db2ec5728a1ff43981f7770657f67011d99a
Baran <Baran.soysuren@gmail.com>
2024-09-15 22:46:15 +0200

    add WebServlet annotation

--- Lesson06_JSTL/src/main/java/com/learnjstl/JSTLServlet.java
+++ Lesson06_JSTL/src/main/java/com/learnjstl/JSTLServlet.java
@@ -1,7 +1,10 @@
 package com.learnjstl;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+
+@WebServlet("/JSTLServlet")
 public class JSTLServlet extends HttpServlet {
 
 }
