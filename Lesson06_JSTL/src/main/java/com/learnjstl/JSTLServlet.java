00c9f97436b7e28cadea10a69099dbf4b6463cbb
Baran <Baran.soysuren@gmail.com>
2024-09-15 22:47:17 +0200

    add doGet method

--- Lesson06_JSTL/src/main/java/com/learnjstl/JSTLServlet.java
+++ Lesson06_JSTL/src/main/java/com/learnjstl/JSTLServlet.java
@@ -2,9 +2,15 @@ package com.learnjstl;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/JSTLServlet")
 public class JSTLServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 
 }
