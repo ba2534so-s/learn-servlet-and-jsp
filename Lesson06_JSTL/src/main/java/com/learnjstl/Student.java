9b2bbb80b3551f28e789e3b7b3a8f38454f9d90e
Baran <Baran.soysuren@gmail.com>
2024-09-16 21:24:00 +0200

    add constructor

--- Lesson06_JSTL/src/main/java/com/learnjstl/Student.java
+++ Lesson06_JSTL/src/main/java/com/learnjstl/Student.java
@@ -5,5 +5,11 @@ public class Student {
 	int studentID;
 	String name;
 	String grade;
+	
+	public Student(int studentID, String name, String grade) {
+		this.studentID = studentID;
+		this.name = name;
+		this.grade = grade;
+	}
 
 }
