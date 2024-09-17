a15e74245bfe8a71891b0328e6dcfdc0ac20c8ab
Baran <Baran.soysuren@gmail.com>
2024-09-17 23:06:10 +0200

    add servlet annotation

--- Lesson09_ServletFilter/src/main/java/com/learnservletfilters/MyServlet.java
+++ Lesson09_ServletFilter/src/main/java/com/learnservletfilters/MyServlet.java
@@ -1,7 +1,9 @@
 package com.learnservletfilters;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+@WebServlet("/addStudent")
 public class MyServlet  extends HttpServlet {
 
 }
