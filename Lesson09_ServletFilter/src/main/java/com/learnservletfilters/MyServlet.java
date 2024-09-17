4744f3cd7f092536e34b97edeeaaca94db7ae40a
Baran <Baran.soysuren@gmail.com>
2024-09-17 23:07:06 +0200

    add doGet method

--- Lesson09_ServletFilter/src/main/java/com/learnservletfilters/MyServlet.java
+++ Lesson09_ServletFilter/src/main/java/com/learnservletfilters/MyServlet.java
@@ -2,8 +2,14 @@ package com.learnservletfilters;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/addStudent")
 public class MyServlet  extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 
 }
