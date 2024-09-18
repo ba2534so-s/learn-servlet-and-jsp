b06af31f0302dea5c47051d50bbd2d2afeecc1f4
Baran <Baran.soysuren@gmail.com>
2024-09-18 02:04:39 +0200

    get session

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
@@ -4,12 +4,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		
+		HttpSession session = request.getSession();
+
 	}
 
 }
