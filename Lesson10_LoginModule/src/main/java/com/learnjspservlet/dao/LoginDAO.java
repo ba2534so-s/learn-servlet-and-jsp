33e0b496dc5335957c863d2d9b2bbbee25d5e12e
Baran <Baran.soysuren@gmail.com>
2024-09-18 22:48:26 +0200

    add database username

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
@@ -3,7 +3,7 @@ package com.learnjspservlet.dao;
 public class LoginDAO  {
 	
 	String url = "jdbc:postgrsql://localhost:5432/learn_jsp";
-	
+	String db_user = "postgres";
 	
 	
 	public boolean checkUser(String username, String password) {
