5e6c0d03545284b711b4256345da1296e5e67872
Baran <Baran.soysuren@gmail.com>
2024-09-18 22:54:00 +0200

    get connection

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
@@ -1,18 +1,22 @@
 package com.learnjspservlet.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class LoginDAO  {
 	
 	String url = "jdbc:postgrsql://localhost:5432/learn_jsp";
-	String db_user = "postgres";
-	String db_pass = "0";
+	String dbUser = "postgres";
+	String dbPass = "0";
 	
-	String login_query = "SELECT * FROM users WHERE username = ? AND password = ?";
+	String loginQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
 	
 	
 	public boolean checkUser(String username, String password) {
 		
 		try {
 			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection(url, dbUser, dbPass);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
