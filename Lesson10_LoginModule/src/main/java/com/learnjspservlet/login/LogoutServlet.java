6073394c2fbb1ae51ebe887f58efbacf48d48026
Baran <Baran.soysuren@gmail.com>
2024-09-18 02:07:21 +0200

    invalidate session data

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
@@ -14,6 +14,7 @@ public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		HttpSession session = request.getSession();
 		session.removeAttribute("username");
+		session.invalidate();
 		response.sendRedirect("login.jsp");
 	}
 
