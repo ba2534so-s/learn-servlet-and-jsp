4b89da2db79a73f55bd2045014aaa2a5b6da4a6b
Baran <Baran.soysuren@gmail.com>
2024-09-18 22:45:59 +0200

    create checkUser method signature

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
@@ -1,5 +1,11 @@
 package com.learnjspservlet.dao;
 
-public class LoginDAO {
+public class LoginDAO  {
+	
+	public boolean checkUser(String username, String password) {
+		
+		
+		return false;
+	}
 
 }
