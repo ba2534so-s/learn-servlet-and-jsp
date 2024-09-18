15ad596f079596ebefaec664fc16765914803674
Baran <Baran.soysuren@gmail.com>
2024-09-18 02:01:03 +0200

    add WebServlet annotation

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
@@ -1,7 +1,9 @@
 package com.learnjspservlet.login;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 
 }
