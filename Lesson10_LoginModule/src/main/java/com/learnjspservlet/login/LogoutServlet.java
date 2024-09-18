91766f021852a95ddfcc379f47a174e1d92086b1
Baran <Baran.soysuren@gmail.com>
2024-09-18 02:02:51 +0200

    add doGet method

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
@@ -2,8 +2,14 @@ package com.learnjspservlet.login;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
 
 }
