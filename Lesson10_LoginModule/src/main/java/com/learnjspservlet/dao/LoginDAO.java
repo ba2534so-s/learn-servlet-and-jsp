045fa574f3d13870baefb185100c18b7f815d4e9
Baran <Baran.soysuren@gmail.com>
2024-09-18 22:48:51 +0200

    add db password

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
@@ -4,6 +4,7 @@ public class LoginDAO  {
 	
 	String url = "jdbc:postgrsql://localhost:5432/learn_jsp";
 	String db_user = "postgres";
+	String db_pass = "0";
 	
 	
 	public boolean checkUser(String username, String password) {
