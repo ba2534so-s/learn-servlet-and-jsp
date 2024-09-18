fdef9757d206a2aebbb6b36a04aad4f420ed20c0
Baran <Baran.soysuren@gmail.com>
2024-09-18 22:48:00 +0200

    add url to db

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/dao/LoginDAO.java
@@ -2,7 +2,13 @@ package com.learnjspservlet.dao;
 
 public class LoginDAO  {
 	
+	String url = "jdbc:postgrsql://localhost:5432/learn_jsp";
+	
+	
+	
 	public boolean checkUser(String username, String password) {
+		
+		
 		
 		
 		return false;
