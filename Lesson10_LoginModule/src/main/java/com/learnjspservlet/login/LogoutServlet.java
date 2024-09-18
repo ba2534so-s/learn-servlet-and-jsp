5bfc915da9a990321b2ad6a49bb13fbf38bd7302
Baran <Baran.soysuren@gmail.com>
2024-09-18 02:05:31 +0200

    redirect user to login page

--- Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
+++ Lesson10_LoginModule/src/main/java/com/learnjspservlet/login/LogoutServlet.java
@@ -1,5 +1,7 @@
 package com.learnjspservlet.login;
 
+import java.io.IOException;
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,9 +11,10 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		HttpSession session = request.getSession();
 		session.removeAttribute("username");
+		response.sendRedirect("login.jsp");
 	}
 
 }
